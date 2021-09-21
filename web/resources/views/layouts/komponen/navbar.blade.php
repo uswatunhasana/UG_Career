0803c6174052499bdf1044f6e150a300151e192f
uswatunhasana <57187364+uswatunhasana@users.noreply.github.com>
2021-09-21 21:43:04 +0700

    Update navbar.blade.php

--- web/resources/views/layouts/komponen/navbar.blade.php
+++ web/resources/views/layouts/komponen/navbar.blade.php
@@ -6,7 +6,7 @@
 			<div class="logo-header">
 				
 				<a href="{{ route('administrator.dashboard') }}" class="logo">
-					<img src="{{ asset('admin') }}/assets/img/logoazzaracopy.svg" alt="navbar brand" class="navbar-brand">
+					<img src="{{ asset('admin') }}/assets/img/logoazzara.svg" alt="navbar brand" class="navbar-brand">
 				</a>
 				<button class="navbar-toggler sidenav-toggler ml-auto" type="button" data-toggle="collapse" data-target="collapse" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon">
