681e29d350c6e4b41beecb6ef375a79c1016ecae
uswatunhasana <57187364+uswatunhasana@users.noreply.github.com>
2021-09-21 22:45:01 +0700

    Create 2021_09_21_151755_create_beritas_table.php

---
+++ web/database/migrations/2021_09_21_151755_create_beritas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBeritasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('beritas', function (Blueprint $table) {
+            $table->increments('id_berita');
+            $table->enum('jenis_berita',['pengumuman','Lowongan Kerja','Internship']);
+            $table->text('konten');
+            $table->binary('foto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('beritas_');
+    }
+}
