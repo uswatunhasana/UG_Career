766629cb60ed48d5c94a4deef14230e78552a978
uswatunhasana <57187364+uswatunhasana@users.noreply.github.com>
2021-09-21 22:45:04 +0700

    Create 2021_09_21_152631_create_prodis_table.php

---
+++ web/database/migrations/2021_09_21_152631_create_prodis_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prodis', function (Blueprint $table) {
+            $table->increments('id_prodi');
+            $table->string('kd_prodi',5);
+            $table->string('nama_prodi',30);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prodis');
+    }
+}
