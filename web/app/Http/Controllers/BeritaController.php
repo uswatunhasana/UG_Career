945d0a67e3d283ea2b36ff7206238aa1fdf6e432
fhia12 <alifhia121099@gmail.com>
2021-09-22 15:46:01 +0700

    Update BeritaController.php

--- web/app/Http/Controllers/BeritaController.php
+++ web/app/Http/Controllers/BeritaController.php
@@ -5,17 +5,17 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Berita;
+// use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class BeritaController extends Controller
 {
-    
     public function index()
     {
         $beritas = Berita::all();
         return view('admin.berita', ['beritas' => $beritas]);
     }
 
-   
     public function create()
     {
         //
@@ -27,48 +27,28 @@ class BeritaController extends Controller
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function show($id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function edit($id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+   
     public function update(Request $request, $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function destroy($id)
     {
-        //
+        DB::table('beritas')->where('id_berita', $id)->delete();
+        return redirect()->back();
     }
 }
