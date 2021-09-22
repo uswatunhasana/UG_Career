91088492078ff946f5825cd5fbbc3d486d4bc4ab
uswatunhasana <57187364+uswatunhasana@users.noreply.github.com>
2021-09-22 16:49:54 +0700

    Update 2021_09_22_042719_create_perusahaans_table.php

--- web/database/migrations/2021_09_22_042719_create_perusahaans_table.php
+++ web/database/migrations/2021_09_22_042719_create_perusahaans_table.php
@@ -31,6 +31,7 @@ class CreatePerusahaansTable extends Migration
 
     public function down()
     {
+        Schema::disableForeignKeyConstraints();
         Schema::dropIfExists('perusahaans');
     }
 }
