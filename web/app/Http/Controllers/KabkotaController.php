dd90d6cb3896e535d3293d79d072b802b26b960c
uswatunhasana <57187364+uswatunhasana@users.noreply.github.com>
2021-09-22 21:37:38 +0700

    Update KabkotaController.php

--- web/app/Http/Controllers/KabkotaController.php
+++ web/app/Http/Controllers/KabkotaController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 use RealRashid\SweetAlert\Facades\Alert;
 use App\Models\Kabkota;
 use App\Models\Provinsi;
@@ -26,6 +27,14 @@ class KabkotaController extends Controller
 
     public function store(Request $request)
     {
+        $rules = array(
+            'kd_kabkota' => 'string|max:6|required',
+        );
+        $validation = Validator::make($request->all(), $rules);
+        if ($validation->fails()) {
+            Alert::error('Invalid Kode Kab/Kota', 'Maksimal 6 Angka');
+            return redirect()->back();
+        }
         $cek_kabkota = Kabkota::where('kd_kabkota', $request->kd_kabkota)->orWhere('nama_kabkota', $request->nama_kabkota)->count();
         if ($cek_kabkota == 0) {
             $kabkota = new Kabkota;
@@ -55,7 +64,27 @@ class KabkotaController extends Controller
    
     public function update(Request $request, $id)
     {
-        //
+        $rules = array(
+            'kd_kabkota' => 'string|max:6|required',
+        );
+        $validation = Validator::make($request->all(), $rules);
+        if ($validation->fails()) {
+            Alert::error('Invalid Kode Kab/Kota', 'Maksimal 6 Angka');
+            return redirect()->back();
+        }
+
+        $update_kabkota = Kabkota::findOrFail($id)
+        ->leftJoin('provinsis', 'provinsis.id', '=', 'kabkotas.id_provinsi')
+        ->where('kabkotas.id', '=', $id)
+        ->select('provinsis.*', 'kabkotas.*')
+        ->first();
+
+        $update_kabkota->id_provinsi = $request->id_provinsi;
+        $update_kabkota->nama_kabkota = $request->nama_kabkota;
+        $update_kabkota->kd_kabkota = $request->kd_kabkota;
+        $update_kabkota->save();
+        Alert::success(' Berhasil Update Data ', ' Silahkan dicek kembali');
+        return redirect()->back();
     }
 
  
