c5cd6ae26da469048789c8907f5db2e588902c44
fhia12 <alifhia121099@gmail.com>
2021-09-22 15:45:49 +0700

    Update DatabaseSeeder.php

--- web/database/seeders/DatabaseSeeder.php
+++ web/database/seeders/DatabaseSeeder.php
@@ -24,6 +24,9 @@ class DatabaseSeeder extends Seeder
         $this->call([
             KabkotaSeeder::class
         ]);
+        $this->call([
+            BeritaSeeder::class
+        ]);
 
     }
 }
