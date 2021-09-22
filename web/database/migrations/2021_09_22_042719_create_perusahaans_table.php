e51927587f19007e84136225843e4a9c7ce80dbc
uswatunhasana <57187364+uswatunhasana@users.noreply.github.com>
2021-09-22 11:46:39 +0700

    Create 2021_09_22_042719_create_perusahaans_table.php

---
+++ web/database/migrations/2021_09_22_042719_create_perusahaans_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePerusahaansTable extends Migration
+{
+
+    public function up()
+    {
+        Schema::create('perusahaans', function (Blueprint $table) {
+            $table->increments('id_perusahaan');
+            $table->string('nama_instansi',30);
+            $table->text('alamat');
+            $table->string('no_telp',15);
+            $table->string('url_web',100);
+            $table->string('nama_cp',25);
+            $table->string('jabatan',25);
+            $table->string('email_cp',25);
+            //relasi user
+            $table->unsignedInteger('id_user')->nullable();
+            $table->foreign('id_user')
+                ->on('users')
+                ->references('id')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('perusahaans');
+    }
+}
