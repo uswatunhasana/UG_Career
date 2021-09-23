b70ca17d8e3d8a0168934502dd169bc3db521232
firdaaviola <firdaaviola17@gmail.com>
2021-09-23 14:29:42 +0700

    Update DatawebController.php

--- web/app/Http/Controllers/DatawebController.php
+++ web/app/Http/Controllers/DatawebController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Dataweb; 
+use App\Models\Dataweb;
+use Illuminate\Support\Facades\Validator;
+use RealRashid\SweetAlert\Facades\Alert; 
 
 class DatawebController extends Controller
 {
@@ -42,7 +44,31 @@ class DatawebController extends Controller
     
     public function update(Request $request, $id)
     {
-        //
+        $rules = array(
+            'nama_web'      => 'string|required',
+            'no_telp'       => 'string|required',
+            'email'         => 'string|required',
+            'alamat'        => 'string|required',
+            'jam_pelayanan' => 'string|required',
+        );
+
+        $validation = Validator::make($request->all(), $rules);
+        if ($validation->fails()) {
+            Alert::error('Invalid Data', 'Data Tidak Boleh Kosong');
+            return redirect()->back();
+        }
+
+            $dataweb = Dataweb::find($id);
+            $dataweb->nama_web      = $request['nama_web'];
+            $dataweb->no_telp       = $request['no_telp'];
+            $dataweb->email         = $request['email'];
+            $dataweb->alamat        = $request['alamat'];
+            $dataweb->jam_pelayanan = $request['jam_pelayanan'];
+            $dataweb->update();
+            $datawebs = Dataweb::all();
+            Alert::success(' Berhasil Update Data ', ' Silahkan dicek kembali');
+            return view('admin.dataweb', compact('datawebs'));
+
     }
 
     
