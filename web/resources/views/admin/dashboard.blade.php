3962c9d5a266d956f3b58f7cfbda193d48957ff5
uswatunhasana <57187364+uswatunhasana@users.noreply.github.com>
2021-09-26 21:52:46 +0700

    Update dashboard.blade.php

--- web/resources/views/admin/dashboard.blade.php
+++ web/resources/views/admin/dashboard.blade.php
@@ -7,14 +7,18 @@
 		<button type="button" class="btn btn-light btn-round btn-page-header-dropdown dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 			<i class="fa fa-ellipsis-h"></i>
 		</button>
-		<div class="dropdown-menu">
+			@php
+            $jumlah_perusahaan = \App\Models\Perusahaan::count();
+			$jumlah_user_alumni = \App\Models\Alumni::();
+            @endphp
+		<!-- <div class="dropdown-menu">
 			<div class="arrow"></div>
 			<a class="dropdown-item" href="#">Action</a>
 			<a class="dropdown-item" href="#">Another action</a>
 			<a class="dropdown-item" href="#">Something else here</a>
 			<div class="dropdown-divider"></div>
 			<a class="dropdown-item" href="#">Separated link</a>
-		</div>
+		</div> -->
 	</div>
 </div>
 <div class="row">
@@ -29,8 +33,8 @@
 					</div>
 					<div class="col col-stats ml-3 ml-sm-0">
 						<div class="numbers">
-							<p class="card-category">Visitors</p>
-							<h4 class="card-title">1,294</h4>
+							<p class="card-category">Jumlah User Almuni</p>
+							<h4 class="card-title">{{ $jumlah_user_alumni }}</h4>
 						</div>
 					</div>
 				</div>
@@ -48,8 +52,8 @@
 					</div>
 					<div class="col col-stats ml-3 ml-sm-0">
 						<div class="numbers">
-							<p class="card-category">Subscribers</p>
-							<h4 class="card-title">1303</h4>
+							<p class="card-category">Jumlah User Perusahaan</p>
+							<h4 class="card-title">{{ $jumlah_perusahaan }}</h4>
 						</div>
 					</div>
 				</div>
@@ -67,7 +71,26 @@
 					</div>
 					<div class="col col-stats ml-3 ml-sm-0">
 						<div class="numbers">
-							<p class="card-category">Sales</p>
+							<p class="card-category">Jumlah Pertanyaan</p>
+							<h4 class="card-title">$ 1,345</h4>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="col-sm-6 col-md-3">
+		<div class="card card-stats card-round">
+			<div class="card-body">
+				<div class="row align-items-center">
+					<div class="col-icon">
+						<div class="icon-big text-center icon-success bubble-shadow-small">
+							<i class="far fa-chart-bar"></i>
+						</div>
+					</div>
+					<div class="col col-stats ml-3 ml-sm-0">
+						<div class="numbers">
+							<p class="card-category">Jumlah Responden Almuni</p>
 							<h4 class="card-title">$ 1,345</h4>
 						</div>
 					</div>
@@ -86,7 +109,26 @@
 					</div>
 					<div class="col col-stats ml-3 ml-sm-0">
 						<div class="numbers">
-							<p class="card-category">Order</p>
+							<p class="card-category">Jumlah Responden Perusahaan</p>
+							<h4 class="card-title">576</h4>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="col-sm-6 col-md-3">
+		<div class="card card-stats card-round">
+			<div class="card-body">
+				<div class="row align-items-center">
+					<div class="col-icon">
+						<div class="icon-big text-center icon-secondary bubble-shadow-small">
+							<i class="far fa-check-circle"></i>
+						</div>
+					</div>
+					<div class="col col-stats ml-3 ml-sm-0">
+						<div class="numbers">
+							<p class="card-category">Jumlah Berita</p>
 							<h4 class="card-title">576</h4>
 						</div>
 					</div>
