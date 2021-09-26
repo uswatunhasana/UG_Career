01a58b690948c68a59c3d2c82a085ab7cfec487e
firdaaviola <firdaaviola17@gmail.com>
2021-09-26 14:54:33 +0700

    Create PertAlumniController.php

---
+++ web/app/Http/Controllers/PertAlumniController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PertAlumniController extends Controller
+{
+    
+    public function index()
+    {
+        return view('admin.pert_alumni');
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    
+    public function store(Request $request)
+    {
+        //
+    }
+
+    
+    public function show($id)
+    {
+        //
+    }
+
+    public function edit($id)
+    {
+        //
+    }
+
+    
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+   
+    public function destroy($id)
+    {
+        //
+    }
+}
