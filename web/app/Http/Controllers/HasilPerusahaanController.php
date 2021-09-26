6a023d0021d4c91de6cca780fadc832780449713
firdaaviola <firdaaviola17@gmail.com>
2021-09-26 14:54:37 +0700

    Create HasilPerusahaanController.php

---
+++ web/app/Http/Controllers/HasilPerusahaanController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class HasilPerusahaanController extends Controller
+{
+    
+    public function index()
+    {
+        return view('admin.hasil_perusahaan');
+    }
+
+    
+    public function create()
+    {
+        //
+    }
+
+    
+    public function store(Request $request)
+    {
+        //
+    }
+
+    
+    public function show($id)
+    {
+        //
+    }
+
+   
+    public function edit($id)
+    {
+        //
+    }
+
+    
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    
+    public function destroy($id)
+    {
+        //
+    }
+}
