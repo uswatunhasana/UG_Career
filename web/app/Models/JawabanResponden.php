8bd7f5eba0dd1f6d5131c7c33742fd59bab7832a
firdaaviola <firdaaviola17@gmail.com>
2021-09-29 20:57:13 +0700

    Update JawabanResponden.php

--- web/app/Models/JawabanResponden.php
+++ web/app/Models/JawabanResponden.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Models\User;
+use App\Models\Pertanyaan;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -13,6 +15,7 @@ class JawabanResponden extends Model
         'id_user',
         'kategori_responden',
         'pertanyaan',
+        'jawaban',
     ];
 
     public function user()
