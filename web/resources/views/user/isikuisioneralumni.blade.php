f1294df8d401ed718abb9b03c2c06c7a0df1a3e2
fhia12 <alifhia121099@gmail.com>
2021-10-04 00:49:58 +0700

    Update isikuisioneralumni.blade.php

--- web/resources/views/user/isikuisioneralumni.blade.php
+++ web/resources/views/user/isikuisioneralumni.blade.php
@@ -5,51 +5,239 @@
 @section('content')
 <main id="main">
 
-    <!-- ======= Breadcrumbs ======= -->
-    <section class="breadcrumbs">
-      <div class="container">
+  <!-- ======= Breadcrumbs ======= -->
+  <section class="breadcrumbs">
+    <div class="container">
 
-        <ol>
-          <li><a href="{{ route('dashboard.user') }}">Home</a></li>
-          <li><a href="{{ route('isikuisionerrr') }}">Isi Kuisioner Alumni</a></li>
-        </ol>
-        <h2>Isi Kuisioner Alumni UG Career</h2>
+      <ol>
+        <li><a href="{{ route('dashboard.user') }}">Home</a></li>
+        <li><a href="{{ route('isikuisionerrr') }}">Isi Kuisioner Alumni</a></li>
+      </ol>
+      <h2>Isi Kuisioner Alumni UG Career</h2>
 
-      </div>
-    </section><!-- End Breadcrumbs -->
+    </div>
+  </section><!-- End Breadcrumbs -->
 
-    <section id="blog" class="blog">
-      <div class="container" data-aos="fade-up">
+  <section id="blog" class="blog">
+    <div class="container" data-aos="fade-up">
 
+      <form role="form" action="" method="POST">
+       @csrf
+       @method('PUT')
        <article class="entry">
+        <!-- Identitas -->
+        <h2 class="entry-title">
+          <a>Bagian 1: Identitas Alumni</a>
+        </h2>
+        
+        <div class="col-md-6">
 
-              <div class="entry-img">
-                <img src="assets/img/blog/blog-1.jpg" alt="" class="img-fluid">
-              </div>
+          <div class="form-group">
+            <label for="largeInput">Nomor Mahasiswa</label>
+            <input type="text" class="form-control form-control" name = "nomormahasiswa" id="nomormahasiswa" value="">
+          </div>
+          <div class="form-group">
+            <label for="largeInput">Kode PT</label>
+            <input type="text" class="form-control form-control" name = "kodept" id="kodept" value="">
+          </div>
+          <div class="form-group">
+            <label for="largeInput">Tahun Lulus</label>
+            <input type="text" class="form-control form-control" name = "tahunlulus" id="tahunlulus" value="">
+          </div>
+          <div class="form-group">
+            <label for="largeInput">Kode Prodi</label>
+            <input type="text" class="form-control form-control" name = "kodeprodi" id="kodeprodi" value="">
+          </div>
+          <div class="form-group">
+            <label for="largeInput">Nama</label>
+            <input type="text" class="form-control form-control" name = "nama" id="nama" value="">
+          </div>
+          <div class="form-group">
+            <label for="largeInput">Nomor Telepon/HP</label>
+            <input type="text" class="form-control form-control" name = "nomortelepon" id="nomortelepon" value="">
+          </div>
+          <div class="form-group">
+            <label for="largeInput">Email</label>
+            <input type="text" class="form-control form-control" name = "email" id="email" value="">
+          </div>
+          <div class="form-group">
+            <label for="largeInput">NIK</label>
+            <input type="text" class="form-control form-control" name = "nik" id="nik" value="">
+          </div>
+          <div class="form-group">
+            <label for="largeInput">NPWP</label>
+            <input type="text" class="form-control form-control" name = "npwp" id="npwp" value="">
+          </div>
+          <br>
 
-              <h2 class="entry-title">
-                <a href="blog-single.html">Dolorum optio tempore voluptas dignissimos cumque fuga qui quibusdam quia</a>
-              </h2>
+          <!-- Tracer Study -->
+          <h2 class="entry-title"><a>Bagian 2: Tracer Study</a></h2>
+          <!-- Kuisioner Wajib -->
+          <h4><a style="color: red;">Kuisioner Wajib</a></h4>
 
-              <div class="entry-meta">
-                <ul>
-                  <li class="d-flex align-items-center"><i class="bi bi-person"></i> <a href="blog-single.html">John Doe</a></li>
-                  <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a href="blog-single.html"><time datetime="2020-01-01">Jan 1, 2020</time></a></li>
-                  <li class="d-flex align-items-center"><i class="bi bi-chat-dots"></i> <a href="blog-single.html">12 Comments</a></li>
-                </ul>
-              </div>
+          <b>1. Jelaskan status Anda saat ini?</b>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+            <label class="form-check-label" for="exampleRadios1">
+              Bekerja (full time/part time)
+            </label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+            <label class="form-check-label" for="exampleRadios2">
+              Belum memungkinkan bekerja
+            </label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+            <label class="form-check-label" for="exampleRadios2">
+              Wiraswasta
+            </label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+            <label class="form-check-label" for="exampleRadios2">
+              Melanjutkan pendidikan
+            </label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+            <label class="form-check-label" for="exampleRadios2">
+              Tidak kerja tapi sedang mencari kerja
+            </label>
+          </div>
+          <br>
 
-              <div class="entry-content">
-                <p>
-                  Similique neque nam consequuntur ad non maxime aliquam quas. Quibusdam animi praesentium. Aliquam et laboriosam eius aut nostrum quidem aliquid dicta.
-                  Et eveniet enim. Qui velit est ea dolorem doloremque deleniti aperiam unde soluta. Est cum et quod quos aut ut et sit sunt. Voluptate porro consequatur assumenda perferendis dolore.
-                </p>
-                <div class="read-more">
-                  <a href="blog-single.html">Submit</a>
-                </div>
-              </div>
+          <b>2. Apakah anda telah mendapatkan pekerjaan <= 6 bulan / termasuk bekerja sebelum lulus?</b>
+            <div class="form-check">
+              <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+              <label class="form-check-label" for="exampleRadios1">
+                Ya
+              </label>
+            </div>
+            <div class="form-group">
+              <label for="largeInput">Dalam berapa bulan anda mendapatkan pekerjaan?</label>
+              <input type="text" class="form-control form-control" name = "nama" id="nama" value="">
+            </div>
+            <div class="form-group">
+              <label for="largeInput">Berapa rata-rata pendapatan anda per bulan? (take home pay)?</label>
+              <input type="text" class="form-control form-control" name = "nama" id="nama" value="">
+            </div>
+            <div class="form-check">
+              <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+              <label class="form-check-label" for="exampleRadios1">
+                Tidak
+              </label>
+            </div>
+            <div class="form-group">
+              <label for="largeInput">Dalam berapa bulan anda mendapatkan pekerjaan?</label>
+              <input type="text" class="form-control form-control" name = "nama" id="nama" value="">
+            </div>
+            <br>
 
-            </article><!-- End blog entry -->
+            <b>3. Dimana lokasi tempat Anda bekerja?</b>
+            <div class="form-group">
+              <label for="largeInput">Provinsi</label>
+              <input type="text" class="form-control form-control" name = "nama" id="nama" value="">
+            </div>
+            <div class="form-group">
+              <label for="largeInput">Kab/Kota</label>
+              <input type="text" class="form-control form-control" name = "nama" id="nama" value="">
+            </div>
+            <br>
+
+            <b>Silahkan download data referensi wilayah</b>
+            <div><a href="#" class="link-primary">Data referensi Provinsi</a></div>
+            <div><a href="#" class="link-primary">Data referensi Kab/Kota</a></div>
+            <br>
+
+            <b>4. Apa jenis perusahaan/instansi/institusi tempat anda bekerja sekarang?</b>
+            <div class="form-check">
+              <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+              <label class="form-check-label" for="exampleRadios1">
+                Instansi pemerintah 
+              </label>
+            </div>
+            <div class="form-check">
+              <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+              <label class="form-check-label" for="exampleRadios1">
+                Organisasi non-profit/Lembaga Swadaya Masyarakat 
+              </label>
+            </div>
+            <div class="form-check">
+              <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+              <label class="form-check-label" for="exampleRadios1">
+                Perusahaan swasta
+              </label>
+            </div>
+            <div class="form-check">
+              <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+              <label class="form-check-label" for="exampleRadios1">
+                Wiraswasta/perusahaan sendiri
+              </label>
+            </div>
+            <div class="form-check">
+              <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+              <label class="form-check-label" for="exampleRadios1">
+                Lainnya, tuliskan:
+              </label>
+            </div>
+            <div class="form-group">
+              <input type="text" class="form-control form-control" name = "nama" id="nama" value="">
+            </div>
+            <div class="form-check">
+              <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+              <label class="form-check-label" for="exampleRadios1">
+                BUMN/BUMD
+              </label>
+            </div>
+            <div class="form-check">
+              <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+              <label class="form-check-label" for="exampleRadios1">
+                Institusi / Organisasi Multirateral
+              </label>
+            </div>
+            <br>
+
+            <div class="form-group">
+              <b>5. Apa nama perusahaan/kantor tempat Anda bekerja?</b>
+              <input type="text" class="form-control form-control" name = "nama" id="nama" value="">
+            </div>
+            <br>
+
+            <b>6. Bila berwiraswasta, apa posisi/jabatan Anda saat ini ?</b>
+            <div class="form-check">
+              <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+              <label class="form-check-label" for="exampleRadios1">
+                Founder 
+              </label>
+            </div>
+            <div class="form-check">
+              <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+              <label class="form-check-label" for="exampleRadios1">
+                Co-Founder 
+              </label>
+            </div>
+            <div class="form-check">
+              <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+              <label class="form-check-label" for="exampleRadios1">
+                Staff
+              </label>
+            </div>
+            <div class="form-check">
+              <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+              <label class="form-check-label" for="exampleRadios1">
+                Freelance/Kerja Lepas
+              </label>
+            </div>
+            <br>
+
+            <p><b>belom kelar</b></p>
+            
+            <h4><a style="color: black;">Kuisioner Opsional</a></h4>
+
+          </article><!-- End blog entry -->
+        </form>
 
       </div>
     </section><!-- End Blog Single Section -->
@@ -57,5 +245,5 @@
   </main><!-- End #main -->
 
   <!-- ======= Footer ======= -->
-  
-@endsection
+
+  @endsection
