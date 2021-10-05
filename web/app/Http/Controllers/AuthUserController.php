f6295558c14ef883554efb9ccfd370ba9d847bf5
firdaaviola <firdaaviola17@gmail.com>
2021-10-05 19:59:59 +0700

    Update AuthUserController.php

--- web/app/Http/Controllers/AuthUserController.php
+++ web/app/Http/Controllers/AuthUserController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -12,7 +12,38 @@ class AuthUserController extends Controller
         return view('user.login');
     }
 
-    public function create()
+    public function postlogin(Request $request)
+    {
+        request()->validate([
+        'username' => 'required',
+        'password' => 'required',
+        ]);
+
+        $credentials = $request->only('username', 'password');
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            if ($user->level == 'alumni') {
+                return redirect()->intended('login/alumni');
+            } else {
+                return redirect()->intended('login');
+            }
+            return redirect('login');
+        }
+        return redirect('login')->withSuccess('Oppes! Silahkan Cek Inputanmu');
+    }
+
+    public function logout(Request $request) {
+        $request->session()->flush();
+        Auth::logout();
+        return Redirect('/');
+    }
+
+    public function registrasi()
+    {
+        return view('user.daftar');
+    }
+
+    public function simpanregistrasi(Request $request)
     {
         return view('user.daftar');
     }
