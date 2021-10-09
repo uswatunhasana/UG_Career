c299dec3ed8e35414d3cfcf88badfdfa9a67b4f4
uswatunhasana <57187364+uswatunhasana@users.noreply.github.com>
2021-10-09 23:26:01 +0700

    Update masterbackend.blade.php

--- web/resources/views/layouts/masterbackend.blade.php
+++ web/resources/views/layouts/masterbackend.blade.php
@@ -4,7 +4,8 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<title>UG Career Administrator | @yield('title')</title>
 	<meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
-	<link rel="icon" href=" {{ asset('admin') }}/assets/img/icon.ico" type="image/x-icon"/>
+	<!-- <link rel="icon" href=" {{ asset('admin') }}/assets/img/icon.ico" type="image/x-icon"/> -->
+	<link href="{{ asset('assets') }}/img/favicongundar.png" rel="icon">
 	
 	<!-- Fonts and icons -->
 	<script src=" {{ asset('admin') }}/assets/js/plugin/webfont/webfont.min.js"></script>
