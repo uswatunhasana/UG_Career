85dada5bc7f60ce62e8f69643eee1d90704b4599
firdaaviola <firdaaviola17@gmail.com>
2021-10-10 22:30:56 +0700

    Update AuthUserController.php

--- web/app/Http/Controllers/AuthUserController.php
+++ web/app/Http/Controllers/AuthUserController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 use App\Models\User;
 use App\Models\Prodi;
+use App\Models\Alumni;
+use App\Models\Perusahaan;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
@@ -74,7 +76,43 @@ class AuthUserController extends Controller
 
     public function store(Request $request)
     {
-        //
+        // $rules = array(
+        //     'password' => 'string|min:8|required',
+        // );
+        // $validation = Validator::make($request->all(), $rules);
+        // if ($validation->fails()) {
+        //     Alert::error('Invalid Data', 'Password min 8 digit');
+        //     return redirect()->back();
+        // }
+        $daftar_alumni = User::where('username', $request->username)->count();
+        if ($daftar_alumni == 0) {
+            $user           = new User;
+            $user->name     = $request->name;
+            $user->username = $request->username;
+            $user->email    = $request->email;
+            $user->email_verified_at  = now();
+            $user->level    = 'alumni';
+            $user->password = Hash::make($request->password);
+            $user->save();
+
+            $get_id_user = DB::getPdo()->lastInsertId();;
+
+            $alumni = new Alumni;
+            $alumni->id_user     =  $get_id_user;
+            $alumni->npm         = $request->npm;
+            $alumni->tahun_masuk = $request->tahun_masuk;
+            $alumni->tahun_lulus = $request->tahun_lulus;
+            $alumni->id_prodi    = $request->id_prodi;
+            $alumni->no_telp     = $request->no_telp;
+            $alumni->nik         = $request->nik;
+            $alumni->created_at  = now();
+            $alumni->updated_at  = now();
+            $alumni->save();
+            Alert::success(' Berhasil Tambah Data ', ' Silahkan Periksa Kembali');
+        } else {
+            Alert::error('Data Akun Sudah Ada ', ' Silahkan coba lagi');
+        }
+        return redirect()->back();
     }
 
     public function show($id)
