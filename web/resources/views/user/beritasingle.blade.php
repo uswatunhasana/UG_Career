12c56c58b563b46bcf31da3e5b8af9a99c2a50ee
fhia12 <alifhia121099@gmail.com>
2021-10-12 05:22:48 +0700

    Update beritasingle.blade.php

--- web/resources/views/user/beritasingle.blade.php
+++ web/resources/views/user/beritasingle.blade.php
@@ -3,90 +3,98 @@
 @section('css')
 @endsection
 @section('content')
-  <main id="main">
+<main id="main">
 
-    <!-- ======= Breadcrumbs ======= -->
-    <section class="breadcrumbs">
-      <div class="container">
-        <ol>
-          <li><a href="{{ route('dashboard.user') }}">Home</a></li>
-          <li><a href="{{ route('beritaall') }}">Berita</a></li>
-        </ol>
-        <h2>Berita UG Career</h2>
+  <!-- ======= Breadcrumbs ======= -->
+  <section class="breadcrumbs">
+    <div class="container">
+      <ol>
+        <li><a href="{{ route('dashboard.user') }}">Home</a></li>
+        <li><a href="{{ route('beritaall') }}">Berita</a></li>
+      </ol>
+      <h2>Berita UG Career</h2>
 
-      </div>
-    </section><!-- End Breadcrumbs -->
+    </div>
+  </section><!-- End Breadcrumbs -->
 
-    <!-- ======= Blog Single Section ======= -->
-    <section id="blog" class="blog">
-      <div class="container" data-aos="fade-up">
+  <!-- ======= Blog Single Section ======= -->
+  <section id="blog" class="blog">
+    <div class="container" data-aos="fade-up">
 
-        <div class="row">
-          <div class="col-lg-8 entries">
-            @foreach($beritas as $berita)
-            <article class="entry entry-single">
+      <div class="row">
+        <div class="col-lg-8 entries">
+          @foreach($beritas as $berita)
+          <article class="entry entry-single">
 
-              <div class="entry-img">
-                <img src="{{ asset('img/'. $berita->foto )}}" alt="" class="img-fluid">
-              </div>
+            <div class="entry-img">
+              <img src="{{ asset('img/'. $berita->foto )}}" alt="" class="img-fluid">
+            </div>
 
-              <h2 class="entry-title">
-                <a href="">{{ $berita->judul_berita }}</a>
-              </h2>
+            <h2 class="entry-title">
+              <a href="">{{ $berita->judul_berita }}</a>
+            </h2>
 
-              <div class="entry-meta">
-                <ul>
-                  <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a href="blog-single.html"><time datetime="2020-01-01">{{ tanggal_indonesia($berita->created_at) }}</time></a></li>
-                </ul>
-              </div>
+            <div class="entry-meta">
+              <ul>
+                <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a href="blog-single.html"><time datetime="2020-01-01">{{ tanggal_indonesia($berita->created_at) }}</time></a></li>
+              </ul>
+            </div>
 
-              <div class="entry-content">
-                <p>{{ $berita->isi_berita }}</p>
-              </div>
+            <div class="entry-content">
+              <p>{{ $berita->isi_berita }}</p>
+            </div>
 
-            </article><!-- End blog entry -->
+          </article><!-- End blog entry -->
 
-            @endforeach
+          @endforeach
 
-          </div><!-- End blog entries list -->
+        </div><!-- End blog entries list -->
 
-          <div class="col-lg-4">
+        <div class="col-lg-4">
 
-            <div class="sidebar">
+          <div class="sidebar">
 
-              <h3 class="sidebar-title">Kategori</h3>
-              <div class="sidebar-item categories">
+            <!-- <h3 class="sidebar-title">Search</h3>
+            <div class="sidebar-item search-form">
+              <form action="/search" method="get">
+                <input type="search" name="search" placeholder="Search" class="form-control">
+                <button type="submit"><i class="bi bi-search"></i></button>
+              </form>
+            </div> --><!-- End sidebar search formn-->
+
+            <h3 class="sidebar-title">Kategori</h3>
+            <div class="sidebar-item categories">
               @foreach( $kategoris as $kategori)
-                <ul>
-                  <li><a href="{{ route('beritakategori.show', $kategori->jenis_berita) }}">{{ $kategori->jenis_berita }} <span> ({{ $kategori->total }})</span></a></li>
-                </ul>
+              <ul>
+                <li><a href="{{ route('beritakategori.show', $kategori->jenis_berita) }}">{{ $kategori->jenis_berita }} <span> ({{ $kategori->total }})</span></a></li>
+              </ul>
               @endforeach
-              </div><!-- End sidebar categories-->
-              
+            </div><!-- End sidebar categories-->
 
-              <h3 class="sidebar-title">Berita Terbaru</h3>
-              <div class="sidebar-item recent-posts">
-                  @foreach( $listberitas as $listberita)
-                <div class="post-item clearfix">
-                  <img src="{{ asset('img/'. $listberita->foto )}}" alt="">
-                  <h4><a href="/UG_Career/berita/{{$listberita->id}}">{{ $listberita->judul_berita }}</a></h4>
-                  <time datetime="2020-01-01">{{ tanggal_indonesia($listberita->created_at) }}</time>
-                </div>
 
-                @endforeach
+            <h3 class="sidebar-title">Berita Terbaru</h3>
+            <div class="sidebar-item recent-posts">
+              @foreach( $listberitas as $listberita)
+              <div class="post-item clearfix">
+                <img src="{{ asset('img/'. $listberita->foto )}}" alt="">
+                <h4><a href="/UG_Career/berita/{{$listberita->id}}">{{ $listberita->judul_berita }}</a></h4>
+                <time datetime="2020-01-01">{{ tanggal_indonesia($listberita->created_at) }}</time>
+              </div>
 
-              </div><!-- End sidebar recent posts-->
-            </div><!-- End sidebar -->
+              @endforeach
 
-          </div><!-- End blog sidebar -->
+            </div><!-- End sidebar recent posts-->
+          </div><!-- End sidebar -->
 
-        </div>
+        </div><!-- End blog sidebar -->
 
       </div>
-    </section><!-- End Blog Single Section -->
 
-  </main><!-- End #main -->
+    </div>
+  </section><!-- End Blog Single Section -->
 
-  <!-- ======= Footer ======= -->
-  
+</main><!-- End #main -->
+
+<!-- ======= Footer ======= -->
+
 @endsection
