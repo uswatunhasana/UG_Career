cc985682be5121cd32a07dee28218987ac4a513b
firdaaviola <firdaaviola17@gmail.com>
2021-10-15 21:33:48 +0700

    Create email.blade.php

---
+++ web/resources/views/user/email.blade.php
@@ -0,0 +1,5 @@
+
+<h3>Halo, user !</h3>
+<p>Berikut adalah informasi akun UG-Tracer anda. Pastikan informasi ini tidak diberikan pada orang selain pemilik akun.</p>
+<p>Username :  </p>
+<p>Password : </p>
