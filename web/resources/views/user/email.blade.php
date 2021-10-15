7d9a597d755deefcbe5b54d1f1fbfbcfaf38ff58
firdaaviola <firdaaviola17@gmail.com>
2021-10-15 23:48:30 +0700

    Update email.blade.php

--- web/resources/views/user/email.blade.php
+++ web/resources/views/user/email.blade.php
@@ -1,5 +1,5 @@
 
-<h3>Halo, user !</h3>
+<h3>Halo, {{$data_user->name}} !</h3>
 <p>Berikut adalah informasi akun UG-Tracer anda. Pastikan informasi ini tidak diberikan pada orang selain pemilik akun.</p>
-<p>Username :  </p>
-<p>Password : </p>
+<p>Username : {{$data_user->username}} </p>
+<p>Password : {{$password}} </p>
