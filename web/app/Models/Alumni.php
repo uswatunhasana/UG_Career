1673e46d8dc614dfd40f1efdce44f0cc7441fbc9
firdaaviola <firdaaviola17@gmail.com>
2021-10-20 00:37:03 +0700

    Update Alumni.php

--- web/app/Models/Alumni.php
+++ web/app/Models/Alumni.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Models;
-
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -15,8 +15,14 @@ class Alumni extends Model
         'nik',
         'no_telp',
     ];
+    
     public function user()
     {
     	return $this->belongsTo(User::class, 'id_user');
+    }
+
+    public function prodi()
+    {
+    	return $this->belongsTo(Prodi::class, 'id_prodi');
     }
 }
