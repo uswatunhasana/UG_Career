c3688a48935ecceda4858d156aac88df768c708f
firdaaviola <firdaaviola17@gmail.com>
2021-10-20 00:37:06 +0700

    Update ProfilController.php

--- web/app/Http/Controllers/ProfilController.php
+++ web/app/Http/Controllers/ProfilController.php
@@ -8,6 +8,8 @@ use App\Models\User;
 use Illuminate\Support\Facades\Validator;
 use RealRashid\SweetAlert\Facades\Alert;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Crypt;
+
 
 class ProfilController extends Controller
 {
@@ -46,27 +48,34 @@ class ProfilController extends Controller
     public function update(Request $request, $id)
     {
         $rules = array(
-            'name'     => 'string|required',
-            'email'    => 'string|required',
-            'password' => 'string|min:8|required',
+            'name'     => 'string',
+            'email'    => 'string',
+            'password' => 'string|min:8',
         );
 
         $validation = Validator::make($request->all(), $rules);
         if ($validation->fails()) {
-            Alert::error('Invalid Data', 'Data Tidak Boleh Kosong');
+            Alert::error('Invalid Data', 'Password Minimal 8 Karakter, kombinasi dari huruf dan angka');
             return redirect()->back();
         }
-
             $user = User::findOrFail($id);
-            $user->name     = $request['name'];
-            $user->username    = $request['username'];
-            $user->email    = $request['email'];
-            $user->password = $request[Hash::make('password')];
-            $user->forget_password = $request[Hash::make('password')];
+            $user->name     = $request->name;
+            $user->username    = $user->username;
+            $user->email    = $request->email;
+            // $user->password = $request[Hash::make('password')];
+            // $user->forget_password = $request[Hash::make('password')];
+            if($request->filled('password')) {
+                $user->password = Hash::make($request->password);
+                $user->forget_password = Crypt::encryptString($request->password);
+            } else {
+                $user->password = $user->password;
+                $user->forget_password = $user->forget_password;
+            }
             $user->update();
-            $user = User::all();
+            // $user = User::all();
             Alert::success(' Berhasil Update Data ', ' Silahkan dicek kembali');
-            return view('admin.dataweb', compact('datawebs'));
+            return redirect()->back();
+
     }
 
     
