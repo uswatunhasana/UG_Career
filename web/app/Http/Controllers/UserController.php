5a95460e713b69e485a05ee137494300f9a505b4
uswatunhasana <57187364+uswatunhasana@users.noreply.github.com>
2021-10-20 20:55:03 +0700

    Update UserController.php

--- web/app/Http/Controllers/UserController.php
+++ web/app/Http/Controllers/UserController.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Crypt;
 use RealRashid\SweetAlert\Facades\Alert;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Crypt;
+
 
 
 class UserController extends Controller
