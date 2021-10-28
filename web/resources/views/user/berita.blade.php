8d29203ef14781f88e15b600c03efd43122f3165
uswatunhasana <57187364+uswatunhasana@users.noreply.github.com>
2021-10-28 16:23:55 +0700

    Update berita.blade.php

--- web/resources/views/user/berita.blade.php
+++ web/resources/views/user/berita.blade.php
@@ -53,13 +53,13 @@
           </article>
           @endforeach<!-- End blog entry -->
 
-          <div class="blog-pagination">
+          <!-- <div class="blog-pagination">
             <ul class="justify-content-center">
               <li><a href="#">1</a></li>
               <li class="active"><a href="#">2</a></li>
               <li><a href="#">3</a></li>
             </ul>
-          </div>
+          </div> -->
 
           <div class="d-flex justify-content-center">
             {!! $beritas->appends(['sort' => 'science-stream'])->links() !!}
