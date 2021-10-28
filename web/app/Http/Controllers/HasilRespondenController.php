11b4df2d109577605321096cbf3a7414a488e7aa
uswatunhasana <57187364+uswatunhasana@users.noreply.github.com>
2021-10-28 16:24:52 +0700

    Update HasilRespondenController.php

--- web/app/Http/Controllers/HasilRespondenController.php
+++ web/app/Http/Controllers/HasilRespondenController.php
@@ -8,6 +8,7 @@ use App\Models\Jawabanresponden;
 use Illuminate\Support\Facades\DB;
 use App\Exports\HasilRespondenDetail;
 use App\Exports\HasilRespondenPerusahaanDetail;
+use Carbon\Carbon;
 use App\Imports\JawabanRespondenImport;
 use Maatwebsite\Excel\Facades\Excel;
 use Illuminate\Support\Facades\Validator;
@@ -50,6 +51,18 @@ class HasilRespondenController extends Controller
     }
 
     public function exportperusahaan(){
+        // $start = Carbon::now()->startOfMonth()->format('Y-m-d H:i:s');
+        // $end = Carbon::now()->endOfMonth()->format('Y-m-d H:i:s');
+
+        // if (request()->date != '') {
+        //     $date = explode(' - ', request()->date);
+        //     $start = Carbon::parse($date[0])->format('Y-m-d') . ' 00:00:01';
+        //     $end = Carbon::parse($date[1])->format('Y-m-d') . ' 23:59:59';
+        // }
+        // $transaksis = HasilAlumni::select('kode_transaksi', 'bayar', 'kembali', 'created_at', DB::raw('sum(total_barang) as total_beli'), 'subtotal', 'user_id', 'daftar_pelanggan_id')
+        // ->groupBy('kode_transaksi', 'bayar', 'kembali', 'created_at', 'subtotal', 'user_id', 'daftar_pelanggan_id')
+        // ->whereBetween(DB::raw('DATE(created_at)'), array($start, $end))
+        // ->get();
         return Excel::download(new HasilRespondenPerusahaanDetail, 'hasilperusahaan.xlsx');
     }
     public function importalumni(Request $request){
@@ -65,15 +78,8 @@ class HasilRespondenController extends Controller
         $nama_file = time() . '_' . $file->getClientOriginalName();
         $file->move('public/file_responden/', $nama_file);
         Excel::import(new JawabanRespondenImport, 'public/file_responden/' . $nama_file);
-        // $user = Auth::user();
-        // $jawaban_responden = new Jawabanresponden();
-        // $jawaban_responden->kategori_responden = 'alumni';
-        // $jawaban_responden->id_user = $user->id;
-        // if ($jawaban_responden->save()) {
-        //     Excel::import(new JawabanRespondenDetailImport, 'public/file_responden/' . $nama_file);
-        // }
-        // Alert::success('sukses', 'Data Hasil Responden Berhasil Diimport!');
-        // return redirect()->back();
+        Alert::success('sukses', 'Data Hasil Responden Berhasil Diimport!');
+        return redirect()->back();
     }
 
     public function update(Request $request, $id)
