781de5b2f6204c013d219b354cfee7b1384f5273
IlucielI <natsu.nana97@gmail.com>
2021-10-28 15:11:31 +0700

    beritaupdate

--- web/app/Http/Controllers/BeritaController.php
+++ web/app/Http/Controllers/BeritaController.php
@@ -11,29 +11,30 @@ use Illuminate\Support\Facades\Validator;
 use RealRashid\SweetAlert\Facades\Alert;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\File;
+
 class BeritaController extends Controller
 {
-    public function index($kategori="pengumuman")
+    public function index($kategori = "pengumuman")
     {
-        $beritas = Berita::where('jenis_berita','=',$kategori)->select('*')->get();
+        $beritas = Berita::where('jenis_berita', '=', $kategori)->select('*')->get();
         return view('admin.berita2', ['beritas' => $beritas]);
     }
 
-    public function jenisberita($kategori="pengumuman")
+    public function jenisberita($kategori = "pengumuman")
     {
-        $beritas = Berita::where('jenis_berita','=',$kategori)->select('*')->get();
+        $beritas = Berita::where('jenis_berita', '=', $kategori)->select('*')->get();
         return view('admin.berita2', ['beritas' => $beritas]);
     }
 
     public function ajaxdetail($id)
     {
-        $data = Berita::where('id','=', $id)->select('*')->get();
+        $data = Berita::where('id', '=', $id)->select('*')->get();
         echo json_encode($data);
     }
 
 
     public function store(Request $request)
-    {    
+    {
         $rules = array(
             'foto' => 'required|mimes:jpeg,png,jpg,gif,svg',
         );
@@ -42,7 +43,7 @@ class BeritaController extends Controller
             Alert::error('Data Tidak Sesuai', 'Silahkan coba lagi');
             return redirect()->back();
         }
-        
+
         $cek_berita = Berita::where('judul_berita', $request->judul_berita)->count();
         if ($cek_berita == 0) {
             $berita = new Berita;
@@ -51,7 +52,7 @@ class BeritaController extends Controller
             $berita->preview_berita = $request->preview_berita;
             $berita->isi_berita = $request->isi_berita;
             $file = $request->file('foto');
-            $nama_file = rand().$file->getClientOriginalName();
+            $nama_file = rand() . $file->getClientOriginalName();
             $file->move('public/img/', $nama_file);
             $berita->foto = $nama_file;
             $berita->save();
@@ -63,7 +64,7 @@ class BeritaController extends Controller
         return redirect()->back();
     }
 
-    
+
     public function show(Request $request)
     {
         // $cek_berita = Berita::where('judul_berita', $request->judul_berita)->count();
@@ -98,7 +99,7 @@ class BeritaController extends Controller
         // return redirect()->back();
     }
 
-    
+
     public function edit($id)
     {
         //
@@ -110,13 +111,13 @@ class BeritaController extends Controller
         $cek_berita = Berita::where('judul_berita', $request->judul_berita)->count();
         if ($cek_berita > 0) {
             $berita = Berita::findOrFail($id);
-            if ($request->hasFile('foto')){
-                $image_path = public_path("public/img/".$berita->foto);
+            if ($request->hasFile('foto')) {
+                $image_path = public_path("public/img/" . $berita->foto);
                 if (File::exists($image_path)) {
                     File::delete($image_path);
                 }
                 $bannerImage = $request->file('foto');
-                $imgName = $bannerImage->getClientOriginalName();
+                $imgName = rand() . $bannerImage->getClientOriginalName();
                 $destinationPath = public_path('public/img/');
                 $bannerImage->move($destinationPath, $imgName);
             } else {
@@ -129,18 +130,17 @@ class BeritaController extends Controller
             // $file = $request->file('foto');
             // $nama_file = rand().$file->getClientOriginalName();
             // $file->move('public/img/', $nama_file);
-            // $berita->foto = $nama_file;
+            $berita->foto = $imgName;
             $berita->save();
 
             Alert::success('Berhasil Tambah Berita', 'Silahkan Periksa Kembali');
-        } 
-        else {
+        } else {
             Alert::error('Judul Berita Sudah Ada', 'Silahkan coba lagi');
         }
         return redirect()->back();
     }
 
-    
+
     public function destroy($id)
     {
         DB::table('beritas')->where('id', $id)->delete();
