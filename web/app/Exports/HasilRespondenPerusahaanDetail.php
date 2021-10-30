63b111101c8f1a90b0441a7202bd307f3af88c29
uswatunhasana <57187364+uswatunhasana@users.noreply.github.com>
2021-10-30 15:21:15 +0700

    Update HasilRespondenPerusahaanDetail.php

--- web/app/Exports/HasilRespondenPerusahaanDetail.php
+++ web/app/Exports/HasilRespondenPerusahaanDetail.php
@@ -21,20 +21,20 @@ class HasilRespondenPerusahaanDetail implements FromView
     public function view(): View
     {
         $jawabanrespondens = Jawabanresponden::where('kategori_responden', '=', 'perusahaan')->where('created_at', ">=", $this->from . ' 00:00:00')->where('created_at', "<=", $this->to . ' 23:59:59')->get();
-        $kd_pertanyaans = Jawabanrespondendetail::leftJoin('jawabanrespondens', 'jawabanrespondens.id', '=', 'jawabanrespondendetails.id_jawabanresponden')->leftJoin('pertanyaans', 'pertanyaans.kd_pertanyaan', '=', 'jawabanrespondendetails.kd_pertanyaan')
-        ->where('jawabanrespondens.kategori_responden', '=', 'perusahaan')->where('pertanyaans.no_tampilan', '!=', null)
-        ->select('jawabanrespondendetails.kd_jawaban', 'pertanyaans.no_tampilan')->orderBy('pertanyaans.no_tampilan')->orderBy('jawabanrespondendetails.kd_jawaban')
-        ->get();
-        // $kd_pertanyaan_querys = Jawabanrespondendetail::leftJoin('jawabanrespondens','jawabanrespondens.id','=','jawabanrespondendetails.id_jawabanresponden')
-        // ->where('jawabanrespondens.kategori_responden','=','perusahaan')
-        // ->select('jawabanrespondendetails.kd_jawaban', 'jawabanrespondendetails.kd_pertanyaan')->orderBy('jawabanrespondendetails.kd_jawaban')
+        // $kd_pertanyaans = Jawabanrespondendetail::leftJoin('jawabanrespondens', 'jawabanrespondens.id', '=', 'jawabanrespondendetails.id_jawabanresponden')->leftJoin('pertanyaans', 'pertanyaans.kd_pertanyaan', '=', 'jawabanrespondendetails.kd_pertanyaan')
+        // ->where('jawabanrespondens.kategori_responden', '=', 'perusahaan')->where('pertanyaans.no_tampilan', '!=', null)
+        // ->select('jawabanrespondendetails.kd_jawaban', 'pertanyaans.no_tampilan')->orderBy('pertanyaans.no_tampilan')->orderBy('jawabanrespondendetails.kd_jawaban')
         // ->get();
-        // $kd_pertanyaans = [];
-        // foreach ($kd_pertanyaan_querys as $query) {
-        //     $kd_pertanyaans[$query->kd_pertanyaan . "-" . $query->kd_jawaban] = $query;
-        // }
-        // ksort($kd_pertanyaans, SORT_NATURAL);
-        // dd($kd_pertanyaans);
+        $kd_pertanyaan_querys = Jawabanrespondendetail::leftJoin('jawabanrespondens','jawabanrespondens.id','=','jawabanrespondendetails.id_jawabanresponden')
+        ->where('jawabanrespondens.kategori_responden','=','perusahaan')
+        ->select('jawabanrespondendetails.kd_jawaban', 'jawabanrespondendetails.kd_pertanyaan')->orderBy('jawabanrespondendetails.kd_jawaban')
+        ->get();
+        $kd_pertanyaans = [];
+        foreach ($kd_pertanyaan_querys as $query) {
+            $kd_pertanyaans[$query->kd_pertanyaan . "-" . $query->kd_jawaban] = $query;
+        }
+        ksort($kd_pertanyaans, SORT_NATURAL);
+        dd($kd_pertanyaans);
         return view('admin.exports.jawabanrespondenperusahaan', compact('jawabanrespondens', 'kd_pertanyaans'));
     }
 }
