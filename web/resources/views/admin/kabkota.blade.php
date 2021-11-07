05da861e8f3100da37565db24e3335a938149b8e
firdaaviola <firdaaviola17@gmail.com>
2021-11-07 12:14:51 +0700

    Update kabkota.blade.php

--- web/resources/views/admin/kabkota.blade.php
+++ web/resources/views/admin/kabkota.blade.php
@@ -66,7 +66,7 @@
 										<div class="col-md-6">
 											<div class="form-group form-group-default">
 												<label><h4><b>Kode Kab/Kota</b></h4></label>
-												<input id="addkodekabkota" type="text" name="kd_kabkota" class="form-control" placeholder="kode kabupaten/kota">
+												<input id="addkodekabkota" type="text" name="kd_kabkota" class="form-control" placeholder="kode kabupaten/kota" required oninvalid="this.setCustomValidity('data tidak boleh kosong')" oninput="setCustomValidity('')">
 											</div>
 										</div>
 									</div>
@@ -74,7 +74,7 @@
 										<div class="col-sm-12">
 											<div class="form-group form-group-default">
 												<label><h4><b>Nama Kab/Kota</b></h4></label>
-												<input id="addkabkota" type="text" name="nama_kabkota" class="form-control" placeholder="nama kabupaten/kota">
+												<input id="addkabkota" type="text" name="nama_kabkota" class="form-control" placeholder="nama kabupaten/kota" required oninvalid="this.setCustomValidity('data tidak boleh kosong')" oninput="setCustomValidity('')">
 											</div>
 										</div>
 									</div>
